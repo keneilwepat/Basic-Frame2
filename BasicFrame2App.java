28c2efe78f073cf879946d9b0487c170a2085edb
keneilwepat <140274289+keneilwepat@users.noreply.github.com>
2023-07-23 13:07:22 +0200

    Add files via upload

---
+++ BasicFrame2App.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basicframe2app;
+
+import za.ac.tut.ui.MySecondFrame;
+
+/**
+ *
+ * @author MATSAKI CHABALALA
+ */
+public class BasicFrame2App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        MySecondFrame myFrame;
+        
+        myFrame = new MySecondFrame();
+        
+    }
+    
+}
